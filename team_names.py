85adee6461bbe4cb2ed4e42065d18306f178ddd4
boruihe600-dot <boruihe600@gmail.com>
2026-06-13 20:54:49 +0800

    Update team_names.py

--- team_names.py
+++ team_names.py
@@ -1,18 +1,23 @@
-EPL_CN = {
+TEAM_NAMES = {
+
+    # 英超
     "Arsenal": "阿森纳",
     "Manchester City": "曼城",
+    "Manchester United": "曼联",
     "Liverpool": "利物浦",
     "Chelsea": "切尔西",
-    "Manchester United": "曼联",
-    "Tottenham": "热刺",
-}
+    "Tottenham Hotspur": "热刺",
+    "Newcastle United": "纽卡斯尔联",
+    "Aston Villa": "阿斯顿维拉",
 
-LA_LIGA_CN = {
+    # 西甲
     "Barcelona": "巴塞罗那",
     "Real Madrid": "皇家马德里",
-}
+    "Atletico Madrid": "马德里竞技",
+    "Villarreal": "比利亚雷亚尔",
 
-CSL_CN = {
+    # 中超
     "Henan FC": "河南队",
     "Shanghai Shenhua": "上海申花",
+    "Beijing Guoan": "北京国安"
 }
