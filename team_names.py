52496c647bb2c8d46f2c4ecfa84ea45c4c8ac7e4
boruihe600-dot <boruihe600@gmail.com>
2026-06-13 20:41:13 +0800

    Create team_names.py

---
+++ team_names.py
@@ -0,0 +1,70 @@
+EPL_CN = {
+
+"Arsenal":"阿森纳",
+"Manchester City":"曼城",
+"Liverpool":"利物浦",
+"Chelsea":"切尔西",
+"Manchester United":"曼联",
+"Newcastle":"纽卡斯尔",
+"Tottenham":"热刺",
+"Aston Villa":"阿斯顿维拉",
+"Brighton":"布莱顿",
+"West Ham":"西汉姆联",
+"Everton":"埃弗顿",
+"Crystal Palace":"水晶宫",
+"Brentford":"布伦特福德",
+"Fulham":"富勒姆",
+"Bournemouth":"伯恩茅斯",
+"Wolves":"狼队",
+"Nottingham Forest":"诺丁汉森林",
+"Leeds":"利兹联",
+"Sunderland":"桑德兰",
+"Burnley":"伯恩利"
+
+}
+
+LA_LIGA_CN = {
+
+"Barcelona":"巴塞罗那",
+"Real Madrid":"皇家马德里",
+"Atletico Madrid":"马德里竞技",
+"Villarreal":"比利亚雷亚尔",
+"Real Betis":"皇家贝蒂斯",
+"Celta Vigo":"塞尔塔",
+"Getafe":"赫塔菲",
+"Rayo Vallecano":"巴列卡诺",
+"Valencia":"瓦伦西亚",
+"Real Sociedad":"皇家社会",
+"Espanyol":"西班牙人",
+"Athletic Club":"毕尔巴鄂竞技",
+"Sevilla":"塞维利亚",
+"Alaves":"阿拉维斯",
+"Elche":"埃尔切",
+"Levante":"莱万特",
+"Osasuna":"奥萨苏纳",
+"Mallorca":"马略卡",
+"Girona":"赫罗纳",
+"Real Oviedo":"皇家奥维耶多"
+
+}
+
+CSL_CN = {
+
+"Shanghai Shenhua":"上海申花",
+"Chengdu Rongcheng":"成都蓉城",
+"Beijing Guoan":"北京国安",
+"Shanghai Port":"上海海港",
+"Shandong Taishan":"山东泰山",
+"Tianjin Jinmen Tiger":"天津津门虎",
+"Zhejiang":"浙江队",
+"Dalian Yingbo":"大连英博",
+"Qingdao West Coast":"青岛西海岸",
+"Henan FC":"河南队",
+"Yunnan Yukun":"云南玉昆",
+"Wuhan Three Towns":"武汉三镇",
+"Shenzhen Peng City":"深圳新鹏城",
+"Meizhou Hakka":"梅州客家",
+"Changchun Yatai":"长春亚泰",
+"Qingdao Hainiu":"青岛海牛"
+
+}
