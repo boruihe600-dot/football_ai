d1d8f2a23c23d48ffd293450093d8fbaa9771f4c
boruihe600-dot <boruihe600@gmail.com>
2026-06-13 19:18:36 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,47 @@
+# ======================
+# 关注球队
+# ======================
+
+FAVORITE_TEAMS = [
+
+    "Manchester City",
+
+    "Henan FC",
+
+    "China",
+
+    "China U23",
+
+    "China U20",
+
+    "China U17"
+
+]
+
+
+# ======================
+# 关注联赛
+# ======================
+
+FAVORITE_LEAGUES = [
+
+    "Premier League",
+
+    "La Liga",
+
+    "Chinese Super League"
+
+]
+
+
+# ======================
+# 世界杯专区
+# ======================
+
+WORLD_CUP_LEAGUES = [
+
+    "World Cup",
+
+    "World Cup - Qualification Asia"
+
+]
