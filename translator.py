bdc53c144a187a39d1350e4f46631128b6d4f8a0
boruihe600-dot <boruihe600@gmail.com>
2026-06-13 21:01:05 +0800

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,4 +1,5 @@
 from team_names import TEAM_NAMES
+from deep_translator import GoogleTranslator
 
 
 def translate_team(name):
@@ -6,5 +7,14 @@ def translate_team(name):
     if name in TEAM_NAMES:
         return TEAM_NAMES[name]
 
-    # 字典里没有
-    return name
+    try:
+        cn = GoogleTranslator(
+            source='auto',
+            target='zh-CN'
+        ).translate(name)
+
+        return cn
+
+    except:
+
+        return name
